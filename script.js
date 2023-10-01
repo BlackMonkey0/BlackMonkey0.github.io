451dddcf213f5c4464b714ea684d0e511932b7f6
BlackMonkey0 <127506167+BlackMonkey0@users.noreply.github.com>
2023-10-02 01:58:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,26 @@
 const totalAmount = document.getElementById('total-amount');
 const transactionList = document.getElementById('transaction-list');
 const toggleTransactionsButton = document.getElementById('toggle-transactions');
+const amountInput = document.getElementById('amount'); // Modificamos la referencia
 const addTransactionButton = document.getElementById('add-transaction');
 const resetButton = document.getElementById('reset-button');
-const amountInput = document.getElementById('amount'); // Obtén la referencia al campo de entrada de cantidad
 
 // Variable para almacenar los ingresos
 let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
 
 // Función para actualizar la lista de ingresos y el total
 function updateTransactions() {
-    transactionList.innerHTML = ''; // Borra la lista actual
+    transactionList.innerHTML = '';
 
-    // Recorre los ingresos y crea elementos de lista
     transactions.forEach((transaction) => {
         const listItem = document.createElement('li');
         listItem.textContent = `${transaction.amount}€ (${transaction.date})`;
         transactionList.appendChild(listItem);
     });
 
-    // Calcula y actualiza el total
     const total = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
     totalAmount.textContent = `${total}€`;
 
-    // Almacena los ingresos en el almacenamiento local
     localStorage.setItem('transactions', JSON.stringify(transactions));
 }
 
@@ -41,24 +38,18 @@ toggleTransactionsButton.addEventListener('click', () => {
 
 // Agrega un nuevo ingreso cuando se hace clic en el botón "Añadir"
 addTransactionButton.addEventListener('click', () => {
-    const amount = parseFloat(amountInput.value); // Obtén la cantidad ingresada desde el campo de entrada
+    const amount = parseFloat(amountInput.value); // Modificamos la forma de obtener la cantidad
     if (!isNaN(amount)) {
-        const date = new Date().toLocaleDateString(); // Obtiene la fecha actual
+        const date = new Date().toLocaleDateString();
 
-        // Agrega el ingreso a la lista
         transactions.push({ amount, date });
 
-        // Actualiza la lista y el total
         updateTransactions();
-
-        // Limpia el campo de entrada de cantidad
-        amountInput.value = '';
     }
 });
 
 // Agrega un controlador de eventos para el botón de reset
 resetButton.addEventListener('click', () => {
-    // Borra todos los ingresos y actualiza la lista y el total
     transactions = [];
     updateTransactions();
 });
