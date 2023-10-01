4f0f7c0deaba772e158e52a207c4a064334885fd
BlackMonkey0 <127506167+BlackMonkey0@users.noreply.github.com>
2023-10-01 23:23:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,23 +11,30 @@ function agregarCantidad() {
         // Obten la fecha actual
         const fecha = new Date().toLocaleDateString();
 
-        // Obten el total ahorrado del almacenamiento local
-        let totalAhorrado = parseFloat(localStorage.getItem("totalAhorrado")) || 0;
+        // Obten el registro de cantidades ingresadas del almacenamiento local
+        let registroGuardado = localStorage.getItem("registro") || "[]";
+        let registro = JSON.parse(registroGuardado);
 
-        // Actualiza el total ahorrado
-        totalAhorrado += cantidad;
+        // Agrega la cantidad y la fecha al registro
+        registro.push({ cantidad, fecha });
 
-        // Actualiza el total ahorrado en el almacenamiento local
-        localStorage.setItem("totalAhorrado", totalAhorrado);
+        // Guarda el registro actualizado en el almacenamiento local
+        localStorage.setItem("registro", JSON.stringify(registro));
 
-        // Registra la cantidad ingresada junto con la fecha
-        const registro = `${cantidad.toFixed(2)} (Fecha: ${fecha})`;
-        const listItem = document.createElement("li");
-        listItem.textContent = registro;
-        registroLista.appendChild(listItem);
+        // Calcula el total ahorrado
+        let totalAhorrado = 0;
+        for (const item of registro) {
+            totalAhorrado += item.cantidad;
+        }
 
         // Actualiza la visualización del total ahorrado
         totalAhorradoElement.textContent = totalAhorrado.toFixed(2);
+
+        // Agrega un registro a la lista
+        const registroActual = `${cantidad.toFixed(2)} (Fecha: ${fecha})`;
+        const listItem = document.createElement("li");
+        listItem.textContent = registroActual;
+        registroLista.appendChild(listItem);
 
         // Borra el contenido del input
         cantidadInput.value = "";
@@ -36,24 +43,31 @@ function agregarCantidad() {
 
 // Función para borrar el registro almacenado
 function borrarRegistro() {
-    localStorage.removeItem("totalAhorrado");
+    localStorage.removeItem("registro");
     totalAhorradoElement.textContent = "0.00";
     registroLista.innerHTML = ""; // Borra la lista
 }
 
 // Carga el registro almacenado desde el almacenamiento local al cargar la página
 window.addEventListener("load", () => {
-    const totalAhorrado = parseFloat(localStorage.getItem("totalAhorrado")) || 0;
+    // Obten el registro almacenado
+    let registroGuardado = localStorage.getItem("registro") || "[]";
+    let registro = JSON.parse(registroGuardado);
+
+    // Calcula el total ahorrado
+    let totalAhorrado = 0;
+    for (const item of registro) {
+        totalAhorrado += item.cantidad;
+    }
+
+    // Actualiza la visualización del total ahorrado
     totalAhorradoElement.textContent = totalAhorrado.toFixed(2);
 
-    // Carga el registro almacenado
-    const registroGuardado = localStorage.getItem("registro");
-    if (registroGuardado) {
-        const registros = registroGuardado.split(",");
-        for (const registro of registros) {
-            const listItem = document.createElement("li");
-            listItem.textContent = registro;
-            registroLista.appendChild(listItem);
-        }
+    // Muestra la lista de registros
+    for (const item of registro) {
+        const registroActual = `${item.cantidad.toFixed(2)} (Fecha: ${item.fecha})`;
+        const listItem = document.createElement("li");
+        listItem.textContent = registroActual;
+        registroLista.appendChild(listItem);
     }
 });
