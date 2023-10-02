6518c8a2ee51fdaee576afd7050110af7b35bb68
BlackMonkey0 <127506167+BlackMonkey0@users.noreply.github.com>
2023-10-02 10:57:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
-document.addEventListener('input', function (e) {
-    if (e.target.nodeName === 'TD') {
-        // Obtiene la fila y columna de la celda modificada
-        const row = e.target.parentElement.rowIndex - 1; // Restamos 1 para omitir la fila de encabezado
-        const col = e.target.cellIndex - 1; // Restamos 1 para omitir la columna de encabezado
+document.addEventListener('DOMContentLoaded', function () {
+    const tableBody = document.querySelector('#spreadsheet tbody');
+    
+    // Genera 365 filas para los días
+    for (let day = 1; day <= 365; day++) {
+        const row = document.createElement('tr');
+        const dayCell = document.createElement('td');
+        dayCell.textContent = `Día ${day}`;
+        row.appendChild(dayCell);
         
-        // Obtiene el total para la fila
-        const rowTotal = Array.from(e.target.parentElement.cells)
-            .slice(1, -1) // Excluye la primera y última columna
-            .map(cell => parseFloat(cell.textContent) || 0) // Convierte el contenido a número o 0
-            .reduce((acc, val) => acc + val, 0);
+        const euroCell = document.createElement('td');
+        euroCell.contentEditable = true;
+        row.appendChild(euroCell);
         
-        // Actualiza el total en la última celda de la fila
-        const totalCell = document.getElementById(`total-${row}`);
-        totalCell.textContent = rowTotal;
+        const totalCell = document.createElement('td');
+        totalCell.textContent = '0';
+        row.appendChild(totalCell);
+        
+        tableBody.appendChild(row);
     }
-});
+
+    // Agrega un controlador de eventos para calcular el total
+    tableBody.addEventListener('input', function (e) {
+        if (e.target.nodeName === 'TD' && e.target.cellIndex === 1) {
+            const row = e.target.parentElement;
+            const euroCells = Array.from(row.cells).slice(1, -1);
+            const totalCell = row.cells[row.cells.length - 1];
+            
+            const total = euroCells.reduce((acc, cell) => {
+                const amount = parseFloat(cell.textContent) || 0;
+                return acc + amount;
+            }, 0);
+            
+            totalCell.textContent = total.toFixed(2);
+        }
+    });
+
+    // Recupera los datos almacenados en el almacenamiento local
+    for (let day = 1; day <= 365; day++) {
+        const euroCell = tableBody.rows[day - 1].cells[1];
+        const totalCell = tableBody.rows[day - 1].cells[2];
+
+        // Recupera los datos guardados en el almacenamiento local
+        const storedData = localStorage.getItem(`day${day}`);
+        if (storedData) {
+            const { euros, total } = JSON.parse(storedData);
+            euroCell.textContent = euros;
+            totalCell.textContent = total;
+        }
+    });
