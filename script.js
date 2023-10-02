c531fdcbc9ccfb272eaadd00993b5b17231aa432
BlackMonkey0 <127506167+BlackMonkey0@users.noreply.github.com>
2023-10-02 11:09:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
     const spreadsheet = document.getElementById('spreadsheet');
-    const generateRowsButton = document.getElementById('generate-rows');
+    const totalAmountCell = document.getElementById('total-amount');
+    
+    spreadsheet.addEventListener('input', function (event) {
+        const targetCell = event.target;
+        const rowIndex = targetCell.parentNode.rowIndex; // Fila actual
+        const columnIndex = targetCell.cellIndex; // Columna actual
 
-    // Agregar un controlador de eventos al botón "Generar Filas"
-    generateRowsButton.addEventListener('click', function () {
-        const tbody = spreadsheet.querySelector('tbody');
-        const rowCount = tbody.rows.length; // Obtener la cantidad actual de filas
-
-        // Definir el número de filas que deseas agregar (por ejemplo, 5)
-        const rowsToAdd = 5;
-
-        // Generar filas adicionales
-        for (let i = 0; i < rowsToAdd; i++) {
-            const newRow = document.createElement('tr');
-            newRow.innerHTML = `
-                <td>${rowCount + i + 1}</td>
-                <td contentEditable="true"></td>
-                <td contentEditable="false"></td>
-            `;
-            tbody.appendChild(newRow);
+        if (columnIndex === 1) { // Si la columna es la de euros (€)
+            updateTotal();
         }
     });
+
+    function updateTotal() {
+        const euroCells = document.querySelectorAll('tbody td:nth-child(2)'); // Celdas de euros
+        let total = 0;
+
+        euroCells.forEach(function (cell) {
+            const euroValue = parseFloat(cell.textContent) || 0;
+            total += euroValue;
+        });
+
+        totalAmountCell.textContent = total.toFixed(2); // Muestra el total con 2 decimales
+    }
 });
