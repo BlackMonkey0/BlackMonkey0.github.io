4cc1d68c347188db651ee9cf98681cbcc81f59f3
BlackMonkey0 <127506167+BlackMonkey0@users.noreply.github.com>
2023-10-02 10:40:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,28 @@ const resetButton = document.getElementById('reset-button');
 // Variable para almacenar los ingresos
 let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
 
-// Función para actualizar el total
+// Función para calcular el total
+function calculateTotal() {
+    return transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
+}
+
+// Función para actualizar el total y almacenarlo en localStorage
 function updateTotal() {
-    const total = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
+    const total = calculateTotal();
     totalAmount.textContent = `${total}€`;
+    localStorage.setItem('total', total);
+}
+
+// Función para actualizar la lista de ingresos
+function updateTransactionList() {
+    const transactionList = document.getElementById('transaction-list');
+    transactionList.innerHTML = '';
+
+    transactions.forEach((transaction) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${transaction.amount}€ (${transaction.date})`;
+        transactionList.appendChild(listItem);
+    });
 }
 
 // Agrega un nuevo ingreso cuando se hace clic en el botón "Añadir"
@@ -23,6 +41,9 @@ addTransactionButton.addEventListener('click', () => {
         // Actualiza el total
         updateTotal();
 
+        // Actualiza la lista de ingresos
+        updateTransactionList();
+
         // Limpia el campo de cantidad
         amountInput.value = '';
     }
@@ -31,9 +52,20 @@ addTransactionButton.addEventListener('click', () => {
 // Agrega un controlador de eventos para el botón de reset
 resetButton.addEventListener('click', () => {
     transactions = [];
+    localStorage.removeItem('total'); // Elimina el total del almacenamiento local
+
     // Actualiza el total
     updateTotal();
+
+    // Actualiza la lista de ingresos
+    updateTransactionList();
 });
 
 // Llama a esta función al cargar la página para mostrar cualquier ingreso previo
-updateTotal();
+updateTransactionList();
+
+// Recupera y muestra el total almacenado en localStorage al cargar la página
+const storedTotal = localStorage.getItem('total');
+if (storedTotal !== null) {
+    totalAmount.textContent = `${storedTotal}€`;
+}
