1001143303c46c7884de61398391fa1b03e66b80
BlackMonkey0 <127506167+BlackMonkey0@users.noreply.github.com>
2023-10-02 08:31:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
-// Obtén referencias a los elementos HTML en la página de historial
+// Obtén referencias a los elementos HTML en la página principal
+const totalAmount = document.getElementById('total-amount');
 const transactionList = document.getElementById('transaction-list');
+const amountInput = document.getElementById('amount');
+const addTransactionButton = document.getElementById('add-transaction');
+const resetButton = document.getElementById('reset-button');
 
-// Función para cargar los registros de ingresos desde el almacenamiento local
-function loadTransactions() {
-    const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+// Variable para almacenar los ingresos
+let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+
+// Función para actualizar la lista de ingresos y el total
+function updateTransactions() {
+    transactionList.innerHTML = '';
 
     transactions.forEach((transaction) => {
         const listItem = document.createElement('li');
         listItem.textContent = `${transaction.amount}€ (${transaction.date})`;
         transactionList.appendChild(listItem);
     });
+
+    const total = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
+    totalAmount.textContent = `${total}€`;
+
+    localStorage.setItem('transactions', JSON.stringify(transactions));
 }
 
-// Llama a esta función al cargar la página de historial
-loadTransactions();
+// Agrega un nuevo ingreso cuando se hace clic en el botón "Añadir"
+addTransactionButton.addEventListener('click', () => {
+    const amount = parseFloat(amountInput.value);
+    if (!isNaN(amount)) {
+        const date = new Date().toLocaleDateString();
+
+        transactions.push({ amount, date });
+
+        updateTransactions();
+    }
+});
+
+// Agrega un controlador de eventos para el botón de reset
+resetButton.addEventListener('click', () => {
+    transactions = [];
+    updateTransactions();
+});
+
+// Llama a esta función al cargar la página para mostrar cualquier ingreso previo
+updateTransactions();
