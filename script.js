a82d13d234b6469e55f1d2bcdd05fd04ad8a1fda
BlackMonkey0 <127506167+BlackMonkey0@users.noreply.github.com>
2023-10-02 11:07:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const tableBody = document.querySelector('#spreadsheet tbody');
-    
-    // Genera 365 filas para los días
-    for (let day = 1; day <= 365; day++) {
-        const row = document.createElement('tr');
-        const dayCell = document.createElement('td');
-        dayCell.textContent = `Día ${day}`;
-        row.appendChild(dayCell);
-        
-        const euroCell = document.createElement('td');
-        euroCell.contentEditable = true;
-        row.appendChild(euroCell);
-        
-        const totalCell = document.createElement('td');
-        totalCell.textContent = '0';
-        row.appendChild(totalCell);
-        
-        tableBody.appendChild(row);
-    }
+    const spreadsheet = document.getElementById('spreadsheet');
+    const generateRowsButton = document.getElementById('generate-rows');
 
-    // Agrega un controlador de eventos para calcular el total
-    tableBody.addEventListener('input', function (e) {
-        if (e.target.nodeName === 'TD' && e.target.cellIndex === 1) {
-            const row = e.target.parentElement;
-            const euroCells = Array.from(row.cells).slice(1, -1);
-            const totalCell = row.cells[row.cells.length - 1];
-            
-            const total = euroCells.reduce((acc, cell) => {
-                const amount = parseFloat(cell.textContent) || 0;
-                return acc + amount;
-            }, 0);
-            
-            totalCell.textContent = total.toFixed(2);
+    // Agregar un controlador de eventos al botón "Generar Filas"
+    generateRowsButton.addEventListener('click', function () {
+        const tbody = spreadsheet.querySelector('tbody');
+        const rowCount = tbody.rows.length; // Obtener la cantidad actual de filas
+
+        // Definir el número de filas que deseas agregar (por ejemplo, 5)
+        const rowsToAdd = 5;
+
+        // Generar filas adicionales
+        for (let i = 0; i < rowsToAdd; i++) {
+            const newRow = document.createElement('tr');
+            newRow.innerHTML = `
+                <td>${rowCount + i + 1}</td>
+                <td contentEditable="true"></td>
+                <td contentEditable="false"></td>
+            `;
+            tbody.appendChild(newRow);
         }
     });
-
-    // Recupera los datos almacenados en el almacenamiento local
-    for (let day = 1; day <= 365; day++) {
-        const euroCell = tableBody.rows[day - 1].cells[1];
-        const totalCell = tableBody.rows[day - 1].cells[2];
-
-        // Recupera los datos guardados en el almacenamiento local
-        const storedData = localStorage.getItem(`day${day}`);
-        if (storedData) {
-            const { euros, total } = JSON.parse(storedData);
-            euroCell.textContent = euros;
-            totalCell.textContent = total;
-        }
-    });
+});
