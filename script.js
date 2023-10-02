a4f499637028d072c908e4402bae823871f30c85
BlackMonkey0 <127506167+BlackMonkey0@users.noreply.github.com>
2023-10-02 02:33:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
-// Obtén referencias a los elementos HTML
+// Obtén referencias a los elementos HTML en la página principal
 const totalAmount = document.getElementById('total-amount');
-const amountInput = document.getElementById('amount'); // Modificamos la referencia
+const transactionList = document.getElementById('transaction-list');
+const amountInput = document.getElementById('amount');
 const addTransactionButton = document.getElementById('add-transaction');
 const resetButton = document.getElementById('reset-button');
+const showHistoryButton = document.getElementById('show-history'); // Botón para mostrar el historial
 
 // Variable para almacenar los ingresos
 let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
 
-// Función para actualizar el total
-function updateTotal() {
+// Función para actualizar la lista de ingresos y el total
+function updateTransactions() {
+    transactionList.innerHTML = '';
+
+    transactions.forEach((transaction) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${transaction.amount}€ (${transaction.date})`;
+        transactionList.appendChild(listItem);
+    });
+
     const total = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
     totalAmount.textContent = `${total}€`;
+
+    localStorage.setItem('transactions', JSON.stringify(transactions));
 }
 
 // Agrega un nuevo ingreso cuando se hace clic en el botón "Añadir"
 addTransactionButton.addEventListener('click', () => {
     const amount = parseFloat(amountInput.value);
     if (!isNaN(amount)) {
-        transactions.push({ amount });
-        amountInput.value = ''; // Limpiar el campo de entrada
-        updateTotal();
+        const date = new Date().toLocaleDateString();
+
+        transactions.push({ amount, date });
+
+        updateTransactions();
     }
 });
 
 // Agrega un controlador de eventos para el botón de reset
 resetButton.addEventListener('click', () => {
     transactions = [];
-    updateTotal();
+    updateTransactions();
 });
 
-// Llama a esta función al cargar la página para mostrar cualquier total previo
-updateTotal();
+// Agrega un controlador de eventos para el botón de mostrar historial
+showHistoryButton.addEventListener('click', () => {
+    // Redirige a la página de historial
+    window.location.href = 'historial.html';
+});
+
+// Llama a esta función al cargar la página para mostrar cualquier ingreso previo
+updateTransactions();
